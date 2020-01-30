5456fb28cda879149600b248f435fadac4a97a66
Teerth Patel <teerth1205@gmail.com>
2020-01-30 03:05:23 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,43 @@
 # code here!
+class School
+  
+  def initialize(name)
+    @name = name
+    @hash = {}
+  end
+  
+  def roster
+    @hash 
+  end 
+  
+  def add_student(stu_name, grade)
+   
+   if @hash[grade] != nil
+  
+    @hash[grade] << stu_name
+    else 
+    @hash[grade] = [stu_name]
+    end 
+  end 
+  
+  def grade(grade)
+    @hash[grade]
+  end 
+  
+  def sort
+    new_hash = {}
+     @hash.each do |key, value|
+       new_hash[key] = value.sort
+    end 
+    
+    
+    new_hash
+      
+     
+ 
+    
+  end 
+        
+        
+    
+end 
